a1ad9c390884b503a5150df1fda1c04b87243406
Colm Britton <colmjude@gmail.com>
2017-04-06 07:43:05 +0100

    add branching for building-a-service or prototying choice

--- app/routes.js
+++ app/routes.js
@@ -8,4 +8,18 @@ router.get('/', function (req, res) {
 
 // add your routes here
 
+// Purpose for using PaaS
+router.get('/survey/building-a-new-service', function (req, res) {
+  // get the answer from the query string (eg. ?over18=false)
+  var purpose = req.query['purpose-q']
+
+  if (purpose === 'hosting a service') {
+    // redirect to the relevant page
+    res.render('survey/building-a-new-service')
+  } else {
+    // if over18 is any other value (or is missing) render the page requested
+    res.redirect('/survey/additional-prototype-features')
+  }
+})
+
 module.exports = router
